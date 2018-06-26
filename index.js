0f1c5ee28c06c9de091285f2eaa778cdb8c3183f
MiaRene15 <kids.hines@icloud.com>
2018-06-26 15:05:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ $('#searchRep').click(function searchTerms() {
 )
 
 
-$.ajax({
-        url: 'https://api.github.com', 
-        dataType: 'json',
-        data: {
-            api_key: '', 
-            q: input, 
-            limit: 1, 
-            rating: 'PG-13'
-        }
 
 
 
