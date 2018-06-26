23d8e46abc0ee00a8e05032afd48d347eaedf129
MiaRene15 <kids.hines@icloud.com>
2018-06-26 15:13:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ let user = $('#searchTerm').val()
 
 $('#searchRep').click(function() {
   let user = $('#searchTerm').val()
-  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => (
-    )
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    
+  }
   
 }
 )
