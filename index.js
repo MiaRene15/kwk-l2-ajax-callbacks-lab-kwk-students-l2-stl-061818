f6a3ab9edd058c8360734f96680ad9ec3997e3bd
MiaRene15 <kids.hines@icloud.com>
2018-06-26 14:48:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 $(document).ready(function (){
-  
+  $.ajax({
+        url: 'https://api.giphy.com/v1/gifs/search', 
+        dataType: 'json',
+        data: {
+            api_key: 'eb1e26943a32445898b2524d4d9877db', 
+            q: input, 
+            limit: 3, 
+            rating: 'PG-13'
+        }
 });
 
 
